75d35fc26ff4d92993d22bafabab57c88cc11526
s13bby <qaws3623@gmail.com>
2026-03-18 13:04:26 +0600

    update lab1

--- lab1/main.py
+++ lab1/main.py
@@ -4,37 +4,18 @@ from bs4 import BeautifulSoup
 
 def parse():
     file = open('Погода февраль Омск.txt', 'w')
-    url = 'https://omsk.nuipogoda.ru/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0-%D0%BD%D0%B0-%D1%84%D0%B5%D0%B2%D1%80%D0%B0%D0%BB%D1%8C'
-    times = []
-    page = requests.get(url)
-    soup = BeautifulSoup(page.text, 'html.parser')
-    block = soup.find_all('td')
-
-    for td in block:
-        if td.has_attr('time'):
-            times.append(td)
-
-    for index in times:
-        if index.find('span', class_='month'):
-            month = "Месяц: " + index.find('span', class_='month').text
-        else:
-            month = ''
-
-        date = index.find('span', class_='number').text
-
-        if index.find('div', class_='i'):
-            weather = index.find('div', class_='i').get('title')
-        else:
-            weather = ''
-
-        day_temp = index.find('span', class_='max').text
-        night_temp = index.find('span', class_='min').text
-
-        file.write(f"{month}\n")
-        file.write(f"Дата: {date}\n")
-        file.write(f"Погода: {weather}\n")
-        file.write(f"Температура днем: {day_temp}\n")
-        file.write(f"Температура ночью: {night_temp}\n")
+    url = "https://pogoda.365c.ru/russia/omsk/m/february"
+ 
+    response = requests.get(url)
+    content = BeautifulSoup(response.text, 'html.parser')
+    date = [date.get_text(strip=True) for date in content.find_all("td", class_="d-name")]
+    temp_day = [temp_day.get_text(strip=True) for temp_day in content.find_all("td", class_="d-temp")]
+    temp_night = [temp_day.get_text(strip=True) for temp_day in content.find_all("td", class_="d-temp4")]
+    file.write("Ферваль\n")
+    for index in range(len(temp_day)):
+        file.write(f"Дата: {date[index]} {date[0]}\n")
+        file.write(f"Температура днем: {temp_day[index]}\n")
+        file.write(f"Температура ночью: {temp_night[index]}\n\n")
 
 if __name__ == '__main__':
     parse()
